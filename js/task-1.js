fcf09e1daf474954ab74c6dcc1ad202b3967977e
Tochka <tochka114@gmail.com>
2024-12-01 21:20:35 +0200

    up

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,14 @@
-function makeTransaction(quantity, pricePerDroid) {
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
+const makeTransaction = (quantity, pricePerDroid) => {
   const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+  return console.log(
+    `You ordered ${quantity} droids worth ${totalPrice} credits!`
+  );
+};
+makeTransaction(5, 300);
--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,12 @@
-function getShippingMessage(country, price, deliveryFee) {
+// function getShippingMessage(country, price, deliveryFee) {
+//   const totalPrice = price + deliveryFee;
+//   return `Shipping to ${country} will cost ${totalPrice} credits`;
+// }
+// console.log(getShippingMessage("Australia", 120, 50));
+// console.log(getShippingMessage("Germany", 80, 20));
+// console.log(getShippingMessage("Sweden", 100, 20));
+const getShippingMessage = (country, price, deliveryFee) => {
   const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+  return console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
+};
+getShippingMessage("Australia", 120, 50);
